3e0a694ed4b27155495ce6f5bec39b7ef1c97b28
VHelbig <43109146+VHelbig@users.noreply.github.com>
2021-11-15 14:43:05 +0100

    Multiple Ultrasonic sensors now working

--- SMPRO3/SMPRO3/main.c
+++ SMPRO3/SMPRO3/main.c
@@ -7,7 +7,7 @@
 
 
 #define F_CPU 16000000
-#define ULTRA_NUMBER 3
+#define ULTRA_NUMBER 2
 //INCLUDES
 #include <avr/io.h>
 #include <stdio.h>
@@ -39,7 +39,7 @@ int get_timer_value(void);
 int get_and_stop_timer(void);
 void trigger_ultra(int index);
 void print_all_timer_values(void);
-
+int read_timer_value(int index);
 
 
 int main(void)
@@ -51,11 +51,24 @@ int main(void)
 	}
 	
 	//Input and Output pins
-    DDRF=0xFF>>(8-ULTRA_NUMBER);
+    DDRF=0xFF;
 	PORTF=0x00;
 	
 	DDRD=0x00; //all input ECho all is connected here
 	PORTD=0x00; // No pull ups
+	
+	// use timer 3 as pwm
+	// no pin output just interreupts
+	TCCR3A=(1<<WGM31)|(1<<WGM30);
+	TCCR3B=(1<<WGM33)|(1<<WGM32)|(1<<CS30)|(1<<CS31);
+	//Top value (25002) in OCR3A:
+	OCR3AH=(uint8_t)(25002>>8);
+	OCR3AL=(uint8_t)(25002);
+	//Compare int at OCRB=24999
+	OCR3BH=(uint8_t)(24999>>8);
+	OCR3BL=(uint8_t)(24999);
+	//enable compare b interrupt and overflow interrupt
+	TIMSK3=(1<<OCIE3B)|(1<<TOIE3);
 	
 	
 	//Interrupts
@@ -71,7 +84,7 @@ int main(void)
     while (1) 
     {
 		//generate Trig signal
-		trigger_ultra(ultra_index);
+		//trigger_ultra(ultra_index);
 		print_all_timer_values();
 		_delay_ms(500);
     }
@@ -114,7 +127,7 @@ int get_timer_value(void){
 }
 
 void trigger_ultra(int index){
-	PORTF=(0x02<<index);
+	PORTF=(0x01<<index);
 	_delay_us(10);
 	PORTF=0;
 }
@@ -125,10 +138,30 @@ void print_all_timer_values(void){
 		printf("Ultrasonic %d: \t%d \t",i,timer_values[i].timer_value);
 		if(timer_values[i].updated>0){
 			printf("updated\n");
+			timer_values[i].updated=0;
 		}else{
 			printf("not updated\n");
 		}
 	}
 	printf("Index: %d\n",ultra_index);
 	
+}
+
+//PWM interrupts
+volatile char index_buffer=0;
+
+ISR(TIMER3_COMPB_vect){
+	//Trigger pin
+	index_buffer=ultra_index;
+	PORTF=(1<<index_buffer);
+}
+
+ISR(TIMER3_OVF_vect){
+	//release pin
+	PORTF=0x00;
+}
+
+int read_timer_value(int index){
+	timer_values[index].updated=0;
+	return timer_values[index].timer_value;
 }
