1f938d6b40f61a56c4737964f07670585e5bd4f3
VHelbig <43109146+VHelbig@users.noreply.github.com>
2021-12-06 08:45:47 +0100

    Added comments

--- SMPRO3/SMPRO3/main.c
+++ SMPRO3/SMPRO3/main.c
@@ -7,29 +7,31 @@
 
 
 #define F_CPU 16000000
-#define ULTRA_NUMBER 4
+#define ULTRA_NUMBER 4	//number if ultrasonic sensors
 
 #include <avr/io.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
-#include "i2cmaster.h"
-#include "usart.h"
-#include "PCA9685.h"
+#include "i2cmaster.h"	//i2c
+#include "usart.h"		//usart library from alin also works on arduino mega
+#include "PCA9685.h"	//motorcontrol board
 
 //STRUCTURES
 typedef struct{
 	int timer_value;
-	char updated;
-}timer_value_t;
+	char updated;		//replace this with timestamp later
+}timer_value_t;			//struct for keeping track of all timer values and if they are up to date
 
 //GLOBAL VARIABLES
 
-//interrupt variables:
+//ultrasonic data array
 volatile timer_value_t timer_values[5];
+//index to currently trigger ultra sonic s.
 volatile int ultra_index=0;
 
+// optocoupler counters
 volatile int optocounter1=0;
 volatile int optocounter2=0;
 
@@ -39,12 +41,12 @@ char stop=0;
 
 
 //FUNCTION PROTOTYPES
-void reset_timer(void);
-int get_timer_value(void);
-int get_and_stop_timer(void);
-void trigger_ultra(int index);
-void print_all_timer_values(void);
-int read_timer_value(int index);
+void reset_timer(void);		//just reset timer1
+int get_timer_value(void);	//just get timer1 value 
+int get_and_stop_timer(void); // get timer value and stop timer1
+void trigger_ultra(int index);	//triggers ultrasonic sensor not recommended, as it blocks the execution for 10us
+void print_all_timer_values(void);	//prints all ultrasonic sensor values and their update status
+int read_timer_value(int index);	//function for getting ultra sonic sensor value from array, wont be needed later
 
 
 int main(void)
@@ -55,21 +57,22 @@ int main(void)
 		timer_values[i].updated=0;
 	}
 	
-	//Input and Output pins
+	//Triger ultra sonic pins
     DDRF=0xFF;
 	PORTF=0x00;
 	
-	DDRD=0x00; //all input ECho all is connected here
+	DDRD=0x00; //Echo all is connected here
 	PORTD=0x00; // No pull ups
 	
-	// use timer 3 as pwm
+	// use timer 3 as pwm generator for ultrasonic sensors
+	//the timer will count up and reset at TOP
 	// no pin output just interreupts
 	TCCR3A=(1<<WGM31)|(1<<WGM30);
 	TCCR3B=(1<<WGM33)|(1<<WGM32)|(1<<CS30)|(1<<CS31);
 	//Top value (25002) in OCR3A:
 	OCR3AH=(uint8_t)(25002>>8);
 	OCR3AL=(uint8_t)(25002);
-	//Compare int at OCRB=24999
+	//Compare interrupt at OCRB=24999
 	OCR3BH=(uint8_t)(24999>>8);
 	OCR3BL=(uint8_t)(24999);
 	//enable compare b interrupt and overflow interrupt
@@ -97,7 +100,7 @@ int main(void)
 	//i2c 
 	i2c_init();
 	motor_init_pwm(PWM_FREQUENCY_50);
-	motor_set_state(M1,CW);
+	
     while (1) 
     {
 		//generate Trig signal
@@ -111,6 +114,7 @@ int main(void)
     }
 }
 
+//Triggered on any edge on ECHO
 ISR(INT2_vect){
 	if((PIND&(1<<PD2))>0){ //rising edge
 		reset_timer();
@@ -169,12 +173,13 @@ void print_all_timer_values(void){
 }
 
 //PWM interrupts
-volatile char index_buffer=0;
+volatile char index_buffer=0; //buffer index, so it is not change during interrupt
 
 ISR(TIMER3_COMPB_vect){
 	//Trigger pin
 	index_buffer=ultra_index;
 	PORTF=(1<<index_buffer);
+	
 	ultra_index++;
 	if(ultra_index>=ULTRA_NUMBER){
 		ultra_index=0;
@@ -197,6 +202,7 @@ int read_timer_value(int index){
 
 ISR(INT4_vect){
 	optocounter1++;
+	
 }
 
 ISR(INT5_vect){
